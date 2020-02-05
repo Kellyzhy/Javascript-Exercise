4fd2b0136586251faaa4f87b789105edf0f66069
Kellyzhy <46937935+Kellyzhy@users.noreply.github.com>
2020-02-05 10:02:37 -0500

    Javascrip Assignment

---
+++ Javascipt Exercise/Javascipt Assignment/script/QueueExample.js
@@ -0,0 +1,43 @@
+﻿function queue() {
+    var items = []
+    var count = 0;
+    this.unshift = function (item) {
+        items.unshift(item)
+        count++;
+    }
+    this.shift = function () {
+        if (count > 0) {
+            items.shift()
+            count--
+        }
+    }
+    this.print = function () {
+        for (var i = 0; i < count; i++) {
+            console.log(items[i]);
+        }
+    }
+}
+
+var s = new queue()
+s.unshift("Burberry")
+s.unshift("Channel")
+s.unshift("Gucci")
+s.shift()
+s.unshift("Givenchy")
+s.unshift("Louis Vitton")
+s.shift()
+console.log(s)
+
+s.print();
+
+var s1 = new queue();
+s1.unshift("CK one")
+s1.unshift("Channel No 5")
+s1.shift()
+s1.unshift("Bulgari")
+s1.unshift("Hermes")
+
+s1.print();
+
+
+
---
+++ Javascipt Exercise/Javascipt Assignment/script/add&substract&multiple&division.js
@@ -0,0 +1,23 @@
+﻿function addNumbers() {
+    var a = parseInt(document.getElementById("fn").value)  //convert into int
+    var b = parseInt(document.getElementById("sn").value)
+    document.getElementById("result").value = a + b
+}
+
+function substrction() {
+    var a = parseInt(document.getElementById("fn").value)  //convert into int
+    var b = parseInt(document.getElementById("sn").value)
+    document.getElementById("result").value = a - b
+}
+
+function multiplication() {
+    var a = parseInt(document.getElementById("fn").value)  //convert into int
+    var b = parseInt(document.getElementById("sn").value)
+    document.getElementById("result").value = a * b
+}
+
+function division() {
+    var a = parseInt(document.getElementById("fn").value)  //convert into int
+    var b = parseInt(document.getElementById("sn").value)
+    document.getElementById("result").value = a / b
+} 
